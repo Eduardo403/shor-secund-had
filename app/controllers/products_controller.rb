962a1cc7b1e89fb279c58f4eef454799d6b791d3
eduardo403 <edua.24h@gmail.com>
2022-12-20 22:38:19 +0000

    edit router add and move form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,35 @@
 class ProductsController < ApplicationController
   def index
-@products= Product.all
+    @products = Product.all
   end
+  #see a new product
   def show
- @product=Product.find(params[:id])
+    @product = Product.find(params[:id])
   end
   def new
-@product=Product.new
+    @product = Product.new
+  end
+  #create a new product
+  def create
+    @product = Product.new(product_params)
 
+    if @product.save
+      redirect_to products_path, notice: "Product was successfully created"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  #Edit Product
+  def edit
+    @product = Product.find(params[:id])
+    if @product.save
+      redirect_to products_path, notice: "Product was update successfully "
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  #helper
+  def product_params
+    params.require(:product).permit(:title, :description, :price)
   end
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,25 +1,54 @@
-require 'test_helper'
+require "test_helper"
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
- test 'render a list of products' do
-get products_path
+  test "render a list of products" do
+    get products_path
 
-assert_response :success
-assert_select '.product',2
-end
-  test 'render a details page' do
-get product_path(products(:ps4))
-assert_response :success
-assert_select '.title','ps4'
-assert_select '.description','en perfecto estado'
-assert_select '.price','120$'
-
-
+    assert_response :success
+    assert_select ".product", 2
   end
-  test 'render a new product form' do
-    get new_products_path
-assert_response :success
-assert_select 'form'
+  test "render a details page" do
+    get product_path(products(:ps4))
+    assert_response :success
+    assert_select ".title", "ps4"
+    assert_select ".description", "en perfecto estado"
+    assert_select ".price", "120$"
   end
+  test "render a new product form" do
+    get new_product_path
+    assert_response :success
+    assert_select "form"
+  end
+
+  test "allow to create a new product" do
+    post products_path,
+         params: {
+           product: {
+             title: "ps4",
+             description: "en perfecto estado",
+             price: 120
+           }
+         }
+
+    assert_redirected_to products_path
+    assert_equal flash[:notice], "Product was successfully created"
+  end
+  test "dose not allow to create a new product with empty fields" do
+    post products_path,
+         params: {
+           product: {
+             title: "",
+             description: "en perfecto estado",
+             price: 120
+           }
+         }
+
+    assert_response :unprocessable_entity
+  end
+  test "render an edit product form" do
+    get edit_product_path(products(:ps4))
+    assert_redirected_to products_path
+    assert_response :success
+    assert_select "form"
   end
 end
